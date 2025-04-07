d3e84da960dcbbddd2b39a999d3341dc2f2a1842
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2025-04-07 09:43:45 +0100

    Creation of base project

---
+++ InstagramStories/InstagramStories/InstagramStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramStoriesApp.swift
+//  InstagramStories
+//
+//  Created by Dmytro Yaremyshyn on 07/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct InstagramStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
