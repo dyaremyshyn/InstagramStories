70d8a23a1a729920c76904abb6c3d6abbc2f2ac6
Dmytro Yaremyshyn <dmytroy@dxspark.com>
2025-04-07 10:24:13 +0100

    Implementation of 'Persistence' service

---
+++ InstagramStories/InstagramStories/Services/PersistenceService.swift
@@ -0,0 +1,39 @@
+//
+//  PersistenceService.swift
+//  InstagramStories
+//
+//  Created by Dmytro Yaremyshyn on 07/04/2025.
+//
+
+import Foundation
+
+class PersistenceService: Persistence {
+    private let seenKey = "seenStories"
+    private let likedKey = "likedStories"
+    
+    func seen(_ storyId: Int) {
+        var seen = getSeenStories()
+        seen.insert(storyId)
+        UserDefaults.standard.set(Array(seen), forKey: seenKey)
+    }
+    
+    func toggleLike(_ storyId: Int) {
+        var liked = getLikedStories()
+        if liked.contains(storyId) {
+            liked.remove(storyId)
+        } else {
+            liked.insert(storyId)
+        }
+        UserDefaults.standard.set(Array(liked), forKey: likedKey)
+    }
+    
+    private func getSeenStories() -> Set<Int> {
+        let array = UserDefaults.standard.array(forKey: seenKey) as? [Int] ?? []
+        return Set(array)
+    }
+    
+    private func getLikedStories() -> Set<Int> {
+        let array = UserDefaults.standard.array(forKey: likedKey) as? [Int] ?? []
+        return Set(array)
+    }
+}
